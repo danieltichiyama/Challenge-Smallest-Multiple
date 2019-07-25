f8259d3159ce8cf69505bd9a8bb6206a4e74ac0b
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-07-24 19:58:33 -1000

    added primeFactorization function, gotta make it work for me

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -6,6 +6,8 @@
  * @return { Number }         Lowest Positive Number that is evenly divisible by all numbers
  *                            between 1 and `ceiling`
  */
+console.time("test");
+
 module.exports = function(ceiling) {
   arrayOfArrays = [];
 
@@ -34,5 +36,39 @@ module.exports = function(ceiling) {
     multiple++;
   }
 
+  console.timeEnd("test");
+
   return lowestCommonMultiple;
 };
+
+//college try
+function primeFactorization(num, arr = []) {
+  var primeFactors = arr;
+  if (num === 2) {
+    return primeFactors.push(num);
+  }
+
+  var isItPrime = true;
+
+  for (i = 2; i < num; i++) {
+    if (num % i === 0) {
+      var y = num / i;
+      isItPrime = false;
+      primeFactorization(i, primeFactors);
+      primeFactorization(y, primeFactors);
+    }
+  }
+  if (isItPrime) {
+    primeFactors.push(num);
+  }
+
+  return primeFactors;
+}
+
+function (number){
+
+}
+
+//4.5ms to beats
+
+console.log(primeFactorization(100));
