411bfab3f4e1d4426a16816f481e2d8bf58b2ed7
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-07-24 21:40:12 -1000

    exchanged array for new function, need to finish

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -6,69 +6,39 @@
  * @return { Number }         Lowest Positive Number that is evenly divisible by all numbers
  *                            between 1 and `ceiling`
  */
-console.time("test");
-
 module.exports = function(ceiling) {
+  function primeFactorization(num, arr = []) {
+    var primeFactors = arr;
+    if (num === 2) {
+      return primeFactors.push(num);
+    }
+
+    var isItPrime = true;
+
+    for (i = 2; i < num; i++) {
+      if (num % i === 0) {
+        var y = num / i;
+        isItPrime = false;
+        primeFactorization(i, primeFactors);
+        primeFactorization(y, primeFactors);
+      }
+    }
+    if (isItPrime) {
+      primeFactors.push(num);
+    }
+
+    return primeFactors;
+  }
+
   arrayOfArrays = [];
 
   for (i = 2; i < ceiling; i++) {
-    let valueArray = [];
-    valueArray[0] = i;
-    arrayOfArrays.push(valueArray);
+    arrayOfArrays.push(primeFactorization(i));
   }
-
-  lowestCommonMultiple = null;
-  multiple = 2;
-
-  while (lowestCommonMultiple === null) {
-    arrayOfArrays.forEach(function(element) {
-      element[element.length] = element[0] * multiple;
-    });
-
-    if (
-      arrayOfArrays.every(function(element) {
-        return element.includes(arrayOfArrays[0][arrayOfArrays[0].length - 1]);
-      })
-    ) {
-      lowestCommonMultiple = arrayOfArrays[0][arrayOfArrays[0].length - 1];
-    }
-
-    multiple++;
-  }
-
-  console.timeEnd("test");
 
   return lowestCommonMultiple;
 };
 
 //college try
-function primeFactorization(num, arr = []) {
-  var primeFactors = arr;
-  if (num === 2) {
-    return primeFactors.push(num);
-  }
-
-  var isItPrime = true;
-
-  for (i = 2; i < num; i++) {
-    if (num % i === 0) {
-      var y = num / i;
-      isItPrime = false;
-      primeFactorization(i, primeFactors);
-      primeFactorization(y, primeFactors);
-    }
-  }
-  if (isItPrime) {
-    primeFactors.push(num);
-  }
-
-  return primeFactors;
-}
-
-function (number){
-
-}
 
 //4.5ms to beats
-
-console.log(primeFactorization(100));
